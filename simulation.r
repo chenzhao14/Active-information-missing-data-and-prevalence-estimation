4bea1f8406acc32058a71c0544edb7b3b9827e39
Chen Zhao <67875852+chenzhao14@users.noreply.github.com>
2022-10-18 13:14:40 -0400

    Create simulation.r

---
+++ simulation.r
@@ -0,0 +1,102 @@
+library(dplyr)
+library(LaplacesDemon)
+library(ggpubr)
+library(ggplot2)
+
+get_population_sympton_disease <- function(n,rho1,rho11,p0) {
+  population <- data.frame(id = 1:n,symptom = rbinom(n,1,rho1),disease=0)
+  rho01 <- p0-rho11
+  rho0 <- 1-rho1
+  rho10 <- rho1-rho11
+  rho00 <- rho0-rho01
+  p0_1 <- rho11/rho1
+  p0_0 <- rho01/rho0
+  sym_group <- filter(population, symptom==1)
+  asym_group <- filter(population, symptom==0)
+  n_1 <- floor(n*rho11)
+  n_0 <- floor(n*rho01)
+  sym_group[sample(nrow(sym_group),n_1),]$disease <-1
+  asym_group[sample(nrow(asym_group),n_0),]$disease <-1
+  population <- rbind(sym_group,asym_group)
+  return(population)
+}
+
+
+test_group <- function(population,pi){
+  a<-split(population,f = population[,c("symptom")])
+  tested_group <- c()
+  for(i in 1:length(pi)){
+    tested_group <- rbind(tested_group,sample_frac(a[[i]],pi[i]))
+  }
+  return(tested_group)
+}
+
+
+get_test_prevalence <- function(n,tested_group){
+  N <- n
+  N_T <- nrow(tested_group)
+  N_T1 <- nrow(filter(tested_group, symptom == 1))
+  N_T0 <- nrow(filter(tested_group, symptom == 0))
+  N_T11 <- nrow(filter(tested_group, symptom == 1, disease ==1))
+  N_T_1 <- nrow(filter(tested_group, disease ==1))
+  N_T01 <- nrow(filter(tested_group, symptom == 0, disease ==1))
+  p_T1s <- N_T1/N_T
+  rho_1_hat <- p_T1s/2*(N_T/N+1)
+  p0_1 <- rho_1_hat*N_T11/N_T1
+  p0_0 <- (1-rho_1_hat)*N_T01/N_T0
+  sample <- N_T_1/N_T
+  res <- p0_1+p0_0
+  return(c(res,sample))
+}
+
+run_model_mar <- function(n,p0,rho1,pi,rho11){
+  population <- get_population_sympton_disease(n,rho1,rho11,p0)
+  tested_group <- test_group(population,pi)
+  test_patient_prev <- get_test_prevalence(n,tested_group)
+  return(test_patient_prev)
+}
+
+repeat_function <- function(m,x){
+  res <- matrix(0,ncol=2,nrow = m)
+  i = 1
+  repeat{
+    if(i>m)
+      break
+    res[i,] <- run_model_mar(n=x,p0=0.2,rho1=0.2,pi=c(0.1,0.9),rho11=0.15)
+    i = i+1
+  }
+  return(res)
+}
+
+m=500
+
+set.seed(114)
+
+outcome.ex3<-sapply(c(1000, 10000, 100000, 1000000), function(i) repeat_function(m=500,x=i))
+
+outcome.ex3.df<-as.data.frame(outcome.ex3)
+
+
+
+p1<-ggplot(outcome.100.df[1:m,], aes(sample=V1))+
+  stat_qq()+
+  ggtitle("QQ-Plot of MAR when population = 10000")
+
+p2<-ggplot(outcome.100.df[1:m,], aes(sample=V2))+
+  stat_qq()+
+  ggtitle("QQ-Plot of MAR when population = 100000")
+
+p3<-ggplot(outcome.100.df[1:m,], aes(sample=V3))+
+  stat_qq()+
+  ggtitle("QQ-Plot of MAR when population = 1000000")
+
+p4<-ggplot(outcome.100.df[1:m,], aes(sample=V4))+
+  stat_qq()+
+  ggtitle("QQ-Plot of MAR when population = 10000000")
+
+
+rho5.qq1<-ggpubr::ggarrange(p1,p2,p3,p4,ncol=2,nrow=2)
+rho5.qq1
+
+
+
